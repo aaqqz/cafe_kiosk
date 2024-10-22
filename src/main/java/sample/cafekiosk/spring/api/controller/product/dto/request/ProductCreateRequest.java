335a58f2ca69969529b5ab716e3d1c282b8a73ca
HG_KIM <sswwx1@naver.com>
2024-10-23 00:24:46 +0900

    presentation layer 테스트

--- src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -1,12 +1,12 @@
 package sample.cafekiosk.spring.api.controller.product.dto.request;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
@@ -15,16 +15,16 @@ import sample.cafekiosk.spring.domain.product.ProductType;
 @NoArgsConstructor
 public class ProductCreateRequest {
 
-    @NotNull
+    @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
 
-    @NotNull
+    @NotNull(message = "상품 판매상태는 필수입니다.")
     private ProductSellingStatus sellingStatus;
 
-    @NotBlank
+    @NotBlank(message = "상품 이름은 필수입니다.")
     private String name;
 
-    @Positive
+    @Positive(message = "상품 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
@@ -35,14 +35,12 @@ public class ProductCreateRequest {
         this.price = price;
     }
 
-    public Product toEntity(String nextProductNumber) {
-        return Product.builder()
-                .productNumber(nextProductNumber)
-                .type(type)
-                .sellingStatus(sellingStatus)
-                .name(name)
-                .price(price)
-                .build();
+    public ProductCreateServiceRequest toServiceRequest() {
+        return ProductCreateServiceRequest.builder()
+            .type(type)
+            .sellingStatus(sellingStatus)
+            .name(name)
+            .price(price)
+            .build();
     }
-
 }
--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -33,7 +34,7 @@ public class ProductService {
     // 1, (트래픽 적다 - db unique 조건 걸고 3회 재시도 )
     // 2, (트래픽 많다 - 상품 번호 자체에 유니크한 값, uuid 활용)
     @Transactional
-    public ProductResponse createProduct(ProductCreateRequest request) {
+    public ProductResponse createProduct(ProductCreateServiceRequest request) {
         String nextProductNumber = createNextProductNumber();
 
         Product product = request.toEntity(nextProductNumber);
